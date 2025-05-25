e70b732cc7604787bc71c2e3b389d5e63036b267
CHEF_CALABRIA <calabriac@uninorte.edu.co>
2025-05-24 22:02:21 -0500

    creando el controlador para Plane

--- src/model/storage/PlaneStorage.java
+++ src/model/storage/PlaneStorage.java
@@ -4,10 +4,33 @@
  */
 package model.storage;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import model.Plane;
+
 /**
  *
  * @author Car
  */
 public class PlaneStorage {
+    
+     //almacenamiento general del sistema para guardar objetos de tipo Plane
+    private static ArrayList <Plane> planes = new ArrayList<>();
+
+    public PlaneStorage() throws IOException {
+        //llenando la lista con el metodo para leer archivos json
+        planes = readPlanes("json/planes.json");
+        //ordenanlo la lista por ids
+        planes.sort(Comparator.comparing(Plane::getId));
+    }
+        
+    public ArrayList <Plane> getPlanes() {
+        return planes;
+    }
+
+    private ArrayList<Plane> readPlanes(String jsonplanesjson) {
+        return null;
+    }
     
 }
