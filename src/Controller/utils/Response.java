fb3fb94ea0c92671d4fdfd1879ae45d816ea23e0
CHEF_CALABRIA <calabriac@uninorte.edu.co>
2025-05-24 21:52:47 -0500

    creando response y status

--- src/Controller/utils/Response.java
+++ src/Controller/utils/Response.java
@@ -9,5 +9,39 @@ package Controller.utils;
  * @author Car
  */
 public class Response {
+     private String message;
+    private int status;
+    private Object object;
+
+    public Response(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
     
+    public Response(String message, int status, Object object) {
+        this.message = message;
+        this.status = status;
+        this.object = object;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+    //haciendo uso del patrón de diseño Prototype 
+    @Override
+    public Response clone() {
+        try {
+            return (Response) super.clone(); // shallow copy
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // no debería pasar
+        }
+    }
 }
