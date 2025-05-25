2443e00cd373e513485b29ccc07b5a938b37bd9b
CHEF_CALABRIA <calabriac@uninorte.edu.co>
2025-05-24 21:49:23 -0500

    ya no se ejecuta en la vista sino en el main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,29 @@
  */
 package main;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+import view.AirportFrame;
+
 /**
  *
  * @author Car
  */
 public class Main {
+    public static void main(String args[]) {
+        System.setProperty("flatlaf.useNativeLibrary", "false");
+
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AirportFrame().setVisible(true);
+            }
+        });
+    }
     
 }
